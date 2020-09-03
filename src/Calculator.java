546108542cb804f2a51a7d9ec49ceaf3b08404e7
Shriya <srbiddala01@bvsd.org>
2020-09-03 10:51:02 -0600

    Generating Random Numbers

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) {
+        //       index 0123456789012345
+
+
+        String str3 = "Sirius Cybernetics Corporation";
+
+
+        System.out.println(str3.substring(7, 12));
+
+
+    }
+}
---
+++ src/Rand.java
@@ -0,0 +1,7 @@
+public class Rand {
+    public static void main(String[] args) {
+        double r  = Math.random();
+        System.out.println(r);
+        }
+    }
+
