74634981634a17a8569de5796adfd2b7780694dc
Shriya <srbiddala01@bvsd.org>
2021-01-24 13:24:52 -0700

    Array

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] school = new String[3];
+        school[0] = "Peak";
+        school[1] = "to"; 
+        school[2] = "Peak";
+        for (int i =0; i<3; i++) {
+            System.out.print(school[i] + " ");
+        }
+    }
+}
