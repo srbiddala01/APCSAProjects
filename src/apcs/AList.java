52f5a21d9c1782277a7fb117e422c45263362359
Shriya <srbiddala01@bvsd.org>
2021-01-26 10:59:46 -0700

    ArrayList

--- src/apcs/AList.java
+++ src/apcs/AList.java
@@ -10,12 +10,43 @@ public class AList {
        // list.add("Hello");
        // list.add(new Integer(5));
        // list.add(new Fraction(1,2));
-        list.add(new Double (20.0));
-        list.add (new Double (12.0));
-        list.add(new Double (3));
-        list.add(16.0);
+       // list.add(new Double (20.0));
+       // list.add (new Double (12.0));
+       // list.add(new Double (3));
+      //  list.add(16.0);
        // list.add(12);
 
+        for (int i = 0; i <= 20; i++){
+            list.add(Math.random());
+        }
+        System.out.println(list);
+
+        for (int i = 0; i < list.size(); i++){
+            Double val = list.get(i);
+            val = i * -1.0;
+            list.set(i, val);
+        }
+        System.out.println(list);
+
+        Double min = list.get(0);
+        for(Double val: list){
+            if (val < min){
+                min = val;
+            }
+        }
+        System.out.println("Minimum is: " + min);
+
+
+        for(int i = 0; i < list.size(); i = i + 2){
+            list.add(i, 0.0);
+        }
+        System.out.println(list);
+
+        for(int i = list.size()-1; i >=0; i--){
+            if (list.get(i) > -0.5){
+                list.remove(i);
+            }
+        }
         System.out.println(list);
     }
 }
